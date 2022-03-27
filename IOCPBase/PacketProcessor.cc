cd8bd1d566b08d6a205d50acf55217e6b2f9587f
smartse0k <smartse0k@naver.com>
2022-03-27 18:21:40 +0900

    PacketProcessor 클래스 작성, 큐잉 및 스레드 처리 구현

---
+++ IOCPBase/PacketProcessor.cc
@@ -0,0 +1,66 @@
+#include "PacketProcessor.h"
+
+namespace phodobit {
+    unsigned int PacketProcessor::processorCount = 0;
+    std::vector<std::thread*> PacketProcessor::threadList;
+
+    unsigned int PacketProcessor::nextEnqueueProcessorIndex = 0;
+    std::vector<std::queue<Packet*>*> PacketProcessor::queueList;
+
+    std::vector<std::mutex*> PacketProcessor::mutexQueueList;
+    std::mutex PacketProcessor::mutexNextEnqueueProcessorIndex;
+
+    void PacketProcessor::createPacketProcessor(int threadCount) {
+        for (int i = 0; i < threadCount; i++) {
+            std::queue<Packet*>* queue = new std::queue<Packet*>();
+            queueList.push_back(queue);
+
+            std::mutex* mutex = new std::mutex();
+            mutexQueueList.push_back(mutex);
+
+            std::thread* thread = new std::thread(processor, i);
+            threadList.push_back(thread);
+        }
+
+        processorCount = threadCount;
+    }
+
+    void PacketProcessor::enqueuePacket(Packet* packet) {
+        // 다음 프로세서 인덱스 획득
+        mutexNextEnqueueProcessorIndex.lock();
+        nextEnqueueProcessorIndex++;
+        if (nextEnqueueProcessorIndex >= processorCount) {
+            nextEnqueueProcessorIndex = 0;
+        }
+        mutexNextEnqueueProcessorIndex.unlock();
+
+        // 프로세서에 패킷 인큐
+        mutexQueueList[nextEnqueueProcessorIndex]->lock();
+        queueList[nextEnqueueProcessorIndex]->push(packet);
+        mutexQueueList[nextEnqueueProcessorIndex]->unlock();
+    }
+
+    // Thread Entry Point
+    void PacketProcessor::processor(const unsigned int processorId) {
+        std::mutex* mutex = mutexQueueList[processorId];
+        std::queue<Packet*>* queue = queueList[processorId];
+
+        while (isRunningAndSleep()) {
+            if (queue->empty()) {
+                continue;
+            }
+
+            std::queue<Packet*> q;
+            mutex->lock();
+            queue->swap(q);
+            mutex->unlock();
+
+            while (!q.empty()) {
+                Packet* packet = q.front();
+                q.pop();
+
+                // TODO : Packet 처리
+            }
+        }
+    }
+}
---
+++ IOCPBase/PacketProcessor.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <vector>
+#include <thread>
+#include <queue>
+#include <mutex>
+#include <chrono>
+
+#include "Packet.h"
+
+// TODO : static이 남발되어 있는데 나중에 객체지향적으로 수정...
+
+namespace phodobit {
+    class PacketProcessor {
+    public:
+        static void createPacketProcessor(int threadCount);
+        static void enqueuePacket(Packet *packet);
+
+        static void processor(const unsigned int processorId);
+        
+        inline static bool isRunningAndSleep() {
+            std::this_thread::sleep_for(std::chrono::milliseconds(1));
+            return true;
+        }
+
+    private:
+        static unsigned int processorCount;
+        static std::vector<std::thread*> threadList;
+
+        static unsigned int nextEnqueueProcessorIndex;
+        static std::vector<std::queue<Packet*>*> queueList;
+
+        static std::vector<std::mutex*> mutexQueueList;
+        static std::mutex mutexNextEnqueueProcessorIndex;
+    };
+}
