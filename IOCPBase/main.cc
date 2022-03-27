79f666986c215a4bc74426325555b50949262669
smartse0k <smartse0k@naver.com>
2022-03-27 19:48:46 +0900

    PacketProcessor 생성 추가

--- IOCPBase/main.cc
+++ IOCPBase/main.cc
@@ -2,7 +2,21 @@
 #include <string>
 
 #include "Logger.h"
+#include "PacketProcessor.h"
 #include "IOCP.h"
+
+void startPacketProcessor() {
+    phodobit::PacketProcessor::createPacketProcessor(4);
+}
+
+void startIOCP() {
+    phodobit::IOCP* iocp = new phodobit::IOCP();
+    iocp->initialize();
+    iocp->bind(9999);
+    iocp->listen();
+    iocp->createAcceptThread(1);
+    iocp->createWorkerThread(2);
+}
 
 void startConsole() {
     phodobit::Logger* logger = phodobit::Logger::getLogger("console")->setLogLevel(phodobit::LogLevel::DEBUG);
@@ -20,16 +34,13 @@ int main(void)
     phodobit::Logger* logger = phodobit::Logger::getLogger("main")->setLogLevel(phodobit::LogLevel::DEBUG);
     logger->debug() << "main()\n";
 
-    logger->info() << "IOCP Base starting\n";
+    logger->info() << "PacketProcessor starting\n";
+    startPacketProcessor();
+    logger->info() << "PacketProcessor started\n";
 
-    phodobit::IOCP *iocp = new phodobit::IOCP();
-    iocp->initialize();
-    iocp->bind(9999);
-    iocp->listen();
-    iocp->createAcceptThread(1);
-    iocp->createWorkerThread(2);
-
-    logger->info() << "IOCP Base started\n";
+    logger->info() << "IOCP starting\n";
+    startIOCP();
+    logger->info() << "IOCP started\n";
 
     startConsole();
 
