36878e10e9be7b5d2ff630c0a3684ad75691a832
smartse0k <smartse0k@naver.com>
2022-03-27 13:44:43 +0900

    IOCP Overlapped 클래스 작성

---
+++ IOCPBase/Overlapped.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <winsock2.h>
+
+constexpr int IOCP_BUFFER_SIZE = 8192;
+
+namespace phodobit {
+	class Overlapped : public OVERLAPPED {
+	public:
+		enum class TYPE {
+			NONE = 0,
+			RECV,
+			SEND
+		};
+
+		Overlapped() {
+			buffer = new char[IOCP_BUFFER_SIZE];
+			memset(buffer, 0, IOCP_BUFFER_SIZE);
+			type = TYPE::NONE;
+			currentBufferSize = 0;
+		}
+
+		~Overlapped() {
+			delete buffer;
+		}
+
+		WSABUF wsaBuf;
+		char *buffer;
+		TYPE type;
+		unsigned int currentBufferSize;
+	};
+}
