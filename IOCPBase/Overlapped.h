f95afbbfac332f2ffc54a172cfb5bff6a5417767
smartse0k <smartse0k@naver.com>
2022-04-05 22:36:44 +0900

    버퍼 사이즈 크기 조정

--- IOCPBase/Overlapped.h
+++ IOCPBase/Overlapped.h
@@ -2,7 +2,12 @@
 
 #include <winsock2.h>
 
-constexpr int IOCP_BUFFER_SIZE = 8192;
+// 현재 Packet은 최대 65535 바이트(uint16 표현범위)이므로,
+// 65536으로 충분해보이지만 65530을 받고 남은 사이즈를 받으려다
+// wsaBuf의 len이 0이 되는 순간 0바이트를 받으려고 시도하는 순간
+// 0바이트를 계속해서 수신하려는 문제가 생길 수 있다.
+// 그렇기 때문에 넉넉하게 x2 정도 크기를 잡아준다.
+constexpr int IOCP_BUFFER_SIZE = 65536 * 2;
 
 namespace phodobit {
     class Overlapped : public OVERLAPPED {
